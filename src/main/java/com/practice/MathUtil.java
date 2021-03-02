d2b786932b813dfc346149ce51b56bd8c6446009
LAKSHMAN <lakshman@LAKSHMANs-MacBook-Pro.local>
2021-03-02 11:17:34 +0530

    made print stmts initcap

--- src/main/java/com/practice/MathUtil.java
+++ src/main/java/com/practice/MathUtil.java
@@ -4,22 +4,22 @@ package com.practice;
 public class MathUtil {
 
     public int add(int a, int b) {
-        System.out.println("sum is: "+(a+b+2));
+        System.out.println("Sum is: "+(a+b+2));
         return a+b+2;
     }
 
     public int diff(int a, int b) {
-        System.out.println("diff is: "+ (a-b));
+        System.out.println("Diff is: "+ (a-b));
         return a-b;
     }
 
     public int mul(int a, int b) {
-        System.out.println("mul is: "+ (a*b));
+        System.out.println("Mul is: "+ (a*b));
         return a*b;
     }
 
     public int div(int a, int b) {
-        System.out.println("div is: "+ (a/b));
+        System.out.println("Div is: "+ (a/b));
         return a/b;
     }
 }
